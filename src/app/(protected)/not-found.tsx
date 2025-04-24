ae39ceb83b6f8e747bf71ed89f7df5d8ad7a20c8
mridxl <mridxl03@gmail.com>
2025-04-24 16:54:20 +0530

    feat: enhance NotFound component with improved layout and messaging

--- src/app/(protected)/not-found.tsx
+++ src/app/(protected)/not-found.tsx
@@ -1,3 +1,13 @@
 export default function NotFound() {
-  return <p>Sorry. The page you are looking for does not exist.</p>;
+  return (
+    <div className="flex h-[calc(100svh-4.5rem)] flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+      <p className="mt-4">
+        Sorry, the page you are looking for does not exist.
+      </p>
+      <a href="/" className="mt-6 underline underline-offset-4">
+        Go back to Home
+      </a>
+    </div>
+  );
 }
--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -1,5 +1,13 @@
-"use client";
-
-export default function ErrorPage() {
-  return <p>Sorry. An error occurred. Please try again later.</p>;
+export default function NotFound() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+      <p className="mt-4">
+        Sorry, an unexpected error has occurred. Please try again later.
+      </p>
+      <a href="/" className="mt-6 underline underline-offset-4">
+        Go back to Home
+      </a>
+    </div>
+  );
 }
