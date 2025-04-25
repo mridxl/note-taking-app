c688ec7d9f56e5561e5dbad61b23e680dc279966
mridxl <mridxl03@gmail.com>
2025-04-25 16:38:59 +0530

    v0.9

--- src/components/header-menu.tsx
+++ src/components/header-menu.tsx
@@ -9,7 +9,11 @@ export default function HeaderMenu({ email }: { email: string | undefined }) {
   const isMobile = useIsMobile();
   return (
     <div className="flex items-center gap-4">
-      {!isMobile && <p className="text-sm">Hello, {email ?? "User"}!</p>}
+      {!isMobile && (
+        <p className="text-sm">
+          <span className="wave">👋</span> Hello, {email ?? "User"}!
+        </p>
+      )}
       <ModeToggle />
       <form action={signout}>
         <Button type="submit" variant="neutral" size="sm">
